69033f3ebaf77ccf519d542f94aa84998a7d4c86
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-02-20 05:43:48 +0300

    commit tree 55923c356cab6f1fde3ec4f050f3b7ce4582a9d7

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 10, 'M', 'Brown', 'Jovial', 1),
+  ('Rowdy', 8, 'F', 'Yellow', 'Friendly', 1),
+  ('Tabitha', 5, 'M', 'Brown', 'Introvert', 1),
+  ('Sergeant Brown', 20, 'M', 'Grey', 'Easygoing', 1),
+  ('Melissa', 2, 'M', 'Black', 'Wiity', 1),
+  ('Grinch', 15, 'M', 'Brown', 'Gentle', 0),
+  ('Wendy', 7, 'F', 'Black', 'Extrovert', 1),
+  (NULL, 4, 'M', 'Brown', 'Silly', 1);
